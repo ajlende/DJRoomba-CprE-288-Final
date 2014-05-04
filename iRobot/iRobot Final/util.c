8d99a625cc91c63f49b77d597aa12042dbde388d
unknown <nmont@co1313-20.ece.iastate.edu>
2014-05-04 16:42:28 -0500

    final push for doxygen commented util.c

--- iRobot/iRobot Final/util.c
+++ iRobot/iRobot Final/util.c
@@ -4,8 +4,7 @@
  * For an overview of how timer based interrupts work, see
  * page 111 and 133-137 of the Atmel Mega128 User Guide
  *
- * @author Zhao Zhang & Chad Nelson
- * @date 06/26/2012
+ * @author Zhao Zhang, Chad Nelson, Nick Montelibano, Alex Lende, Chris Chafe, Nic Dubois, Ben Williams
  */
 
 #include <avr/io.h>
@@ -17,35 +16,42 @@
 #define MEDIUM_SPEED 150
 #define SLOW_SPEED 100
 
-// Global used for interrupt driven delay functions
+//Global used for interrupt driven delay functions
 volatile unsigned int timer2_tick;
 void timer2_start(char unit);
 void timer2_stop();
 
-// Times recorded from ISR(TIMER1_CAPT_vect) and the flag used with is
+//Times recorded from ISR(TIMER1_CAPT_vect) and the flag used with is
 volatile unsigned current_time = 0;
 volatile int update_flag = 0;
 
-// Initialize the IR distance sensor
+///Initialize the IR distance sensor
+/************************************************************************/
+/* initializes the Analog to Digtal Converter by setting a prescalar
+   of 128 and setting the reverrence voltage to 2.56 V					*/
+/************************************************************************/
 void ADC_init(void) {
-	// Enable ADC
+	
+	//Enable ADC
 	ADCSRA |= (1 << ADEN);
+
 
 	// ADC Prescaler setup - divide the clock speed by 128
 	ADCSRA |= 0x07;
 
 	// Set reference voltage to 2.56V
 	ADMUX |= (1 << REFS1) | (1 << REFS0);
-	
-	// Make the result left adjusted
-	// ADMUX |= _BV(ADLAR);
 }
 
 
 
-
-
-// Read the output from the ADC, currently using channel 2
+///Read the output from the ADC, currently using channel 2
+/************************************************************************/
+/* By taking in an ADC channel, starts converting by setting the ADSC in 
+   ADCSRA. Once done converting, returns the converted value
+   @param channel for the ADC to be used
+   @return raw value from the ADC										*/
+/************************************************************************/
 unsigned int ADC_read(char channel) {
 	
 	ADMUX |= (channel & 0x1F);
@@ -58,21 +64,23 @@ unsigned int ADC_read(char channel) {
 	return ADC;
 }
 
-// Calculate the distance from ADC result
+///Calculate the distance from ADC result in centimeters
+/************************************************************************/
+/* Takes in value from ADC and returns the calculated centimeter 
+   equivalent
+   @param DigitalOutput raw data from ADC_read
+   @return calculated Centimeter Value									*/
+/************************************************************************/
 float calcCm(unsigned int DigitalOutput) {
-	/*float AnalogOutput = ((float) DigitalOutput) * 2.56/1023.0;
-	float Distance = 1/(AnalogOutput - 0.42); 
-	//Output = 1/(distance + .42)
 	
-	return Distance;*/
-	
-	return 46000/(5.0 + 3.9* ((float)DigitalOutput));
-	
-	
-	
+	return 46000/(5.0 + 3.9* ((float)DigitalOutput));	
+
 }
 
 /// Blocks for a specified number of milliseconds
+/************************************************************************/
+/* @param time_val time in seconds for the program to wait				*/
+/************************************************************************/
 void wait_ms(unsigned int time_val) {
 	//Seting OC value for time requested
 	OCR2=250; 				//Clock is 16 MHz. At a prescaler of 64, 250 timer ticks = 1ms.
@@ -85,137 +93,10 @@ void wait_ms(unsigned int time_val) {
 	timer2_stop();
 }
 
-void song_init()
-{
-
-
-oi_byte_tx(140);
-oi_byte_tx(1);
-oi_byte_tx(24);
-oi_byte_tx(67);
-oi_byte_tx(16);
-oi_byte_tx(67);
-oi_byte_tx(16);
-oi_byte_tx(63);
-oi_byte_tx(16);
-oi_byte_tx(60);
-oi_byte_tx(32);
-oi_byte_tx(60);
-oi_byte_tx(32);
-oi_byte_tx(65);
-oi_byte_tx(32);
-oi_byte_tx(65);
-oi_byte_tx(32);
-oi_byte_tx(65);
-oi_byte_tx(16);
-oi_byte_tx(69);
-oi_byte_tx(16);
-oi_byte_tx(69);
-oi_byte_tx(16);
-oi_byte_tx(70);
-oi_byte_tx(16);
-oi_byte_tx(72);
-oi_byte_tx(16);
-oi_byte_tx(70);
-oi_byte_tx(16);
-oi_byte_tx(70);
-oi_byte_tx(16);
-oi_byte_tx(65);
-oi_byte_tx(16);
-oi_byte_tx(62);
-oi_byte_tx(32);
-oi_byte_tx(62);
-oi_byte_tx(32);
-oi_byte_tx(67);
-oi_byte_tx(32);
-oi_byte_tx(67);
-oi_byte_tx(32);
-oi_byte_tx(67);
-oi_byte_tx(16);
-oi_byte_tx(65);
-oi_byte_tx(16);
-oi_byte_tx(65);
-oi_byte_tx(16);
-oi_byte_tx(67);
-oi_byte_tx(16);
-oi_byte_tx(65);
-oi_byte_tx(16);
-
-
-/*
-oi_byte_tx(140);
-oi_byte_tx(0);
-oi_byte_tx(33);
-oi_byte_tx(55);
-oi_byte_tx(32);
-oi_byte_tx(57);
-oi_byte_tx(32);
-oi_byte_tx(55);
-oi_byte_tx(32);
-oi_byte_tx(57);
-oi_byte_tx(32);
-oi_byte_tx(55);
-oi_byte_tx(48);
-oi_byte_tx(57);
-oi_byte_tx(16);
-oi_byte_tx(55);
-oi_byte_tx(32);
-oi_byte_tx(57);
-oi_byte_tx(32);
-oi_byte_tx(58);
-oi_byte_tx(32);
-oi_byte_tx(60);
-oi_byte_tx(32);
-oi_byte_tx(58);
-oi_byte_tx(32);
-oi_byte_tx(60);
-oi_byte_tx(32);
-oi_byte_tx(58);
-oi_byte_tx(48);
-oi_byte_tx(60);
-oi_byte_tx(16);
-oi_byte_tx(58);
-oi_byte_tx(32);
-oi_byte_tx(60);
-oi_byte_tx(32);
-oi_byte_tx(63);
-oi_byte_tx(32);
-oi_byte_tx(65);
-oi_byte_tx(32);
-oi_byte_tx(63);
-oi_byte_tx(32);
-oi_byte_tx(65);
-oi_byte_tx(32);
-oi_byte_tx(63);
-oi_byte_tx(48);
-oi_byte_tx(65);
-oi_byte_tx(16);
-oi_byte_tx(63);
-oi_byte_tx(32);
-oi_byte_tx(65);
-oi_byte_tx(32);
-oi_byte_tx(60);
-oi_byte_tx(16);
-oi_byte_tx(60);
-oi_byte_tx(16);
-oi_byte_tx(60);
-oi_byte_tx(16);
-oi_byte_tx(60);
-oi_byte_tx(16);
-oi_byte_tx(58);
-oi_byte_tx(32);
-oi_byte_tx(60);
-oi_byte_tx(64);
-oi_byte_tx(48);
-oi_byte_tx(16);
-oi_byte_tx(48);
-oi_byte_tx(16);
-oi_byte_tx(48);
-oi_byte_tx(16);
-*/
-}
-
-// Start timer2
+/// Start timer2
+/************************************************************************/
+/* @param unit, 0 for slow, 1 for fast                                  */
+/************************************************************************/
 void timer2_start(char unit) {
 	timer2_tick=0;
 	if ( unit == 0 ) { 		//unit = 0 is for slow 
@@ -230,14 +111,14 @@ void timer2_start(char unit) {
 }
 
 
-// Stop timer2
+/// Stop timer2
 void timer2_stop() {
 	TIMSK&=~0b10000000;		//Disabling O.C. Interrupt for Timer2
 	TCCR2&=0b01111111;		//Clearing O.C. settings
 }
 
 
-// Interrupt handler (runs every 1 ms)
+/// Interrupt handler (runs every 1 ms)
 ISR (TIMER2_COMP_vect) {
 	timer2_tick++;
 }
@@ -292,6 +173,13 @@ void  move_stepper_motor_by_step(int num_steps, int direction) {
 	
 }
 
+/// initializes ping sensor
+/************************************************************************/
+/* First clears Waveform Generation Mode WGM11 and WGM10, then Clears 
+WGM12 and WGM13 but.  Sets Noise Canceler, edge select to first be rising
+edge. Because we're using a large prescalar we don't 
+enable the input capture overflow interrupt */
+/************************************************************************/
 void ping_init(void)
 {
 	// Clear the Waveform Generation Mode WGM11 (bit 1) and WGM10 (bit 0). Everything else should also be 0. 
@@ -302,24 +190,23 @@ void ping_init(void)
 	// Edge Select (bit 6): 0 = falling edge and 1 = rising edge 
 	// clock io / 256 (From prescalar) (bits 0-2)
 	TCCR1B = 0b11000100;
-	
-	// Force Output Compare on channel A, B, or C (bits 5-7) all should be set to 0. We shouldn't have to change this.
-	// TCCR1C = 0;
-	
+
 	// Timer/Counter 1 Input Capture Enable (bit 5) 
 	// Timer/Counter 1 Overflow Interrupt Enable (bit 2). Use a big prescalar, so we won't need to use this.
 	// Use these to disable the interrupt when outputting, and then enable when we are listening for a response.
 	TIMSK = 0b00000000;
 	
-	
-	// ICR1 = 0;
-	// PD4 = 1;
-	
-	// ICF1 = ICP1 Input Capture Flag
-	
 }
 
-// Reads the current distance on the sonar
+/// Reads the current distance on the sonar
+/************************************************************************/
+/* First sends pulse to sonar to initiate sonar retreival, then polls while
+the flag is still set. Uses interrupts to caputure rising edge and falling 
+edge time and uses the difference to calculate the time the pulse took. 
+Finally calls time2dist() with the time as the parameter to return the cm
+distance calculated
+@return cm distance calculated							                */
+/************************************************************************/
 float ping_read(void)
 {
 	unsigned int time;
@@ -357,14 +244,14 @@ float ping_read(void)
 	
 }
 
-// Gets called when we have a rising or falling edge depending on TCCR1B bit 6
+/// Gets called when we have a rising or falling edge depending on TCCR1B bit 6
 ISR(TIMER1_CAPT_vect)
 {
 		current_time = ICR1;
 		update_flag = !update_flag;
 }
 
-// send a pulse on PD4
+/// send a pulse on PD4
 void send_pulse(void)
 {
 	DDRD |= 0x10; // set PD4 as output
@@ -374,36 +261,31 @@ void send_pulse(void)
 	DDRD &= 0xEF; // set PD4 as input	
 }
 
-// Converts the time that we measure into a distance in centimeters, takes in count from read
+/// Converts the time that we measure into a distance in centimeters, takes in count from read
+/************************************************************************/
+/* Converts and int time from ping_read() to an float centimeter distance
+@param time time it took for the ping_read() function to recieve the pulse
+@return distance in centimeters					                        */
+/************************************************************************/
 float time2dist(unsigned int time)
 {
-	//OLD EQUATION
-	/*unsigned int ClockFrequency = 62500;
-	
-	// distance = rate * time
-	return 34000.0 * (((float) Cycles)/ClockFrequency)/3.0;*/
-	
-	//New Equation
 	return .277 * ((float) time) - 31.944;
 }
 
+///turns the servo motor a specific degrees
+/************************************************************************/
+/* Adjusts the motor by creating a pulse width then sending it to OCR3B to
+turn the servo motor to a specified degree.
+@param degrees the integer value between 0 and 180 used to create the pulse width to turn the servo motor. */
+/************************************************************************/
 void servo_turn(int degrees)
 {
-	// The servo only moves from 0 to 180 degrees.
-	//if (degrees > 180 || degrees < 0) return;
-	
-	// For Robot #3
-	//unsigned int pulse_width = 375 * (180 - (degrees * 104/181 + 37))/90; // pulse width in cycles
-	
-	
-	// For robot #4
+	// calibration for robot #4
 	unsigned int pulse_width = (0.00944 * ((float) degrees) + 0.49) * 250; // pulse width in cycles
-	
-	//For Robot #7
-	//unsigned int pulse_width = (0.00920 * ((float) degrees) + 0.46) * 250;
-	
+
 	// calculate pulse width in cycles
 	OCR3B = pulse_width-1; // set pulse width
+
 	// you need to call wait_ms() here to enforce a delay for the servo to
 	/// move to the position
 	wait_ms(20);
@@ -473,7 +355,16 @@ char read_push_buttons(void) {
 }
 
 //Movement Functions
-//Move forward contains all other functions, allows for error checking while moving
+
+///Move forward contains all other functions, allows for error checking while moving
+/************************************************************************/
+/* Moves the iRobot Create platform a specified value of centimeters, uses the oi_t struct to 
+update its position. While moving it checks for cliff edge sensors, wheel drop sensors,
+line detector sensors, and bumper sensors, responding accordingly for each occurrence.
+@param *sensor nd oi_t struct pointer to update and check values from the oi_t iRobot Create platform
+@param centimeters integer value, in centimeters, of how far the robot should move.
+@return value based on what was happening when the robot stopped moving. i.e. what interrupted its movement*/
+/************************************************************************/
 char move_forward(oi_t *sensor, int centimeters)
 {
 	int sum = 0;
@@ -481,16 +372,15 @@ char move_forward(oi_t *sensor, int centimeters)
 	char outputString[25];
 	float cent_moved;
 	char sensorData[60];
-	
-	//lprintf("Debug 1\n");
-	
+
 	oi_set_wheels(MEDIUM_SPEED, MEDIUM_SPEED); // move forward
-	//lprintf("Debug 2\n");
+
 	while (sum < centimeters*10)
 	{
 		oi_update(sensor);
 		sum += sensor->distance;
-		//lprintf("%d\n", sum);
+
+		//left bumper trigger
 		if(sensor->bumper_left)
 		{
 			oi_set_wheels(0, 0); // stop
@@ -500,10 +390,11 @@ char move_forward(oi_t *sensor, int centimeters)
 			USART_SendString(sensorData);
 
 			move_backward(sensor,10);
-			//turn_clockwise(sensor,45);
 			
 			return 1;
 		}
+		
+		//right bumper trigger
 		else if(sensor->bumper_right)
 		{
 			oi_set_wheels(0, 0); // stop
@@ -513,12 +404,11 @@ char move_forward(oi_t *sensor, int centimeters)
 			USART_SendString(sensorData);
 
 			move_backward(sensor,10);
-			//turn_counterclockwise(sensor,45);
-			
 			
 			return 2;
 		}
 		
+		//frontleft line detected
 		else if((sensor->cliff_frontleft_signal > 800))
 		{
 			
@@ -530,6 +420,7 @@ char move_forward(oi_t *sensor, int centimeters)
 			return 3;
 		}
 		
+		//frontright line detected
 		else if((sensor->cliff_frontright_signal > 800))
 		{
 
@@ -541,6 +432,7 @@ char move_forward(oi_t *sensor, int centimeters)
 			return 3;
 		}
 
+		//right line detected
 		else if((sensor->cliff_right_signal > 800))
 		{
 
@@ -552,6 +444,7 @@ char move_forward(oi_t *sensor, int centimeters)
 			return 3;
 		}
 
+		//left line detected
 		else if((sensor->cliff_left_signal > 800))
 		{
 
@@ -562,7 +455,8 @@ char move_forward(oi_t *sensor, int centimeters)
 			USART_SendString(sensorData);
 			return 3;
 		}
-								
+		
+		//cliff detected						
 		else if((sensor->wheeldrop_caster == 1) || (sensor->wheeldrop_left == 1) || (sensor->wheeldrop_right == 1))
 		{
 
@@ -570,11 +464,11 @@ char move_forward(oi_t *sensor, int centimeters)
 			sprintf(sensorData, "Cliff Edge Detected: Right Sensor: %d Front Right: %d Front Left%d Left: %d | distance moved: %.1f cm", (int) sensor->cliff_frontright_signal, (int) sensor->cliff_frontright_signal, (int) sensor->cliff_frontleft_signal, (int) sensor->cliff_frontleft_signal, cent_moved);
 			USART_SendString(sensorData);
 			lprintf("Cliff Edge Detected");
-			// move_backward(sensor,10);
 			oi_set_wheels(0,0);
 			return 4;
 		}
 		
+		// cliff detected front left
 		else if (sensor->cliff_frontleft_signal < 10) 
 		{
 
@@ -584,6 +478,8 @@ char move_forward(oi_t *sensor, int centimeters)
 			oi_set_wheels(0,0);
 			return 5;
 		}
+		
+		//cliff detected
 		else if (sensor->cliff_frontright_signal < 10)
 		{
 
@@ -593,6 +489,8 @@ char move_forward(oi_t *sensor, int centimeters)
 			oi_set_wheels(0,0);
 			return 6;
 		}
+		
+		// cliff detected left
 		else if (sensor->cliff_left_signal < 10) 
 		{
 
@@ -602,6 +500,8 @@ char move_forward(oi_t *sensor, int centimeters)
 			oi_set_wheels(0,0);
 			return 7;
 		}
+		
+		//cliff detected right
 		else if (sensor->cliff_right_signal < 10)
 		{
 
@@ -623,7 +523,14 @@ char move_forward(oi_t *sensor, int centimeters)
 	return 0;
 }
 
-
+///Moves iRobot Create platform backwards a specified value of centimeters
+/************************************************************************/
+/* Moves robot a specified value backwards by setting the wheels to move
+backwards and stopping once oi_t sensor calculates its final location. 
+Disregards all sensor while moving backwards                            
+@param *sensor an oi_t struct pointer to update and check values from the oi_t iRobot Create platform
+@param centimeters integer value, in centimeters, of how far the robot should move.
+/************************************************************************/
 void move_backward(oi_t *sensor, int centimeters)
 {
 	char sensorData[60];
@@ -642,6 +549,15 @@ void move_backward(oi_t *sensor, int centimeters)
 	USART_SendString(sensorData);
 }
 
+/// turns clockwise by a given degree
+/************************************************************************/
+/* Uses constants MEDIM_SPEED to set the left wheel to move backwards and
+the right wheel to move forwards. Turns until the oi_t sensor angle is
+equal to the given degrees. Checks cliff sensors and line detectors while 
+turning, stops and responds accordingly for each sensor trigger.
+@param *sensor an oi_t struct pointer to update and check values from the oi_t iRobot Create platform
+@param degrees integer value of what degree the iRobot Create platform should turn to.*/
+/************************************************************************/
 void turn_clockwise(oi_t *sensor, int degrees)
 {
 	int sum = 0;
@@ -703,6 +619,16 @@ void turn_clockwise(oi_t *sensor, int degrees)
 	USART_SendString(turnMessage);
 }
 
+
+/// turns clockwise by a given degree
+/************************************************************************/
+/* Uses constants MEDIM_SPEED to set the left wheel to move backwards and
+the right wheel to move forwards. Turns until the oi_t sensor angle is
+equal to the given degrees. Checks cliff sensors and line detectors while
+turning, stops and responds accordingly for each sensor trigger.
+@param *sensor an oi_t struct pointer to update and check values from the oi_t iRobot Create platform
+@param degrees integer value of what degree the iRobot Create platform should turn to.*/
+/************************************************************************/
 void turn_counterclockwise(oi_t *sensor, int degrees)
 {	char sensorData[60];
 	int sum = 0;
@@ -755,11 +681,14 @@ void turn_counterclockwise(oi_t *sensor, int degrees)
 	}
 	
 	oi_set_wheels(0, 0); // stop
-	//degrees -= 10;
 	sprintf(sensorData,"Turned left %d degrees", sum);
 	USART_SendString(sensorData);
 }
 
+///sends all sensor data back through USART
+/************************************************************************/
+/*@param *sensor an oi_t struct pointer to update and check values from the oi_t iRobot Create platform*/
+/************************************************************************/
 void reportData(oi_t *sensor)
 {
 	char senDat[100];
@@ -768,5 +697,69 @@ void reportData(oi_t *sensor)
 	sprintf(senDat, "Cliff Left: %d | Cliff FrontLeft: %d | Cliff FrontRight: %d | Cliff Right: %d | Wheel Drop Left: %d | Wheel Drop Middle: %d | Wheel Drop Left: %d", sensor->cliff_left_signal, sensor->cliff_frontleft_signal, sensor->cliff_frontright_signal, sensor->cliff_right_signal,sensor->wheeldrop_left, sensor->wheeldrop_caster, sensor->wheeldrop_right);
 	USART_SendString(senDat);
 
+}
+
+/// Plays song 'Take on me'
+/************************************************************************/
+/* Uses the oi_byte_tx() function from openInterface() that writes a byte
+of information to the Create platform through serial. The first number,
+140, is the code to start a song. Then next number marks the song number,
+and every other number alternates between indicating the note pitch followed
+by the note length. */
+/************************************************************************/
+void song_init()
+{
+
+	oi_byte_tx(140);
+	oi_byte_tx(1);
+	oi_byte_tx(24);
+	oi_byte_tx(67);
+	oi_byte_tx(16);
+	oi_byte_tx(67);
+	oi_byte_tx(16);
+	oi_byte_tx(63);
+	oi_byte_tx(16);
+	oi_byte_tx(60);
+	oi_byte_tx(32);
+	oi_byte_tx(60);
+	oi_byte_tx(32);
+	oi_byte_tx(65);
+	oi_byte_tx(32);
+	oi_byte_tx(65);
+	oi_byte_tx(32);
+	oi_byte_tx(65);
+	oi_byte_tx(16);
+	oi_byte_tx(69);
+	oi_byte_tx(16);
+	oi_byte_tx(69);
+	oi_byte_tx(16);
+	oi_byte_tx(70);
+	oi_byte_tx(16);
+	oi_byte_tx(72);
+	oi_byte_tx(16);
+	oi_byte_tx(70);
+	oi_byte_tx(16);
+	oi_byte_tx(70);
+	oi_byte_tx(16);
+	oi_byte_tx(65);
+	oi_byte_tx(16);
+	oi_byte_tx(62);
+	oi_byte_tx(32);
+	oi_byte_tx(62);
+	oi_byte_tx(32);
+	oi_byte_tx(67);
+	oi_byte_tx(32);
+	oi_byte_tx(67);
+	oi_byte_tx(32);
+	oi_byte_tx(67);
+	oi_byte_tx(16);
+	oi_byte_tx(65);
+	oi_byte_tx(16);
+	oi_byte_tx(65);
+	oi_byte_tx(16);
+	oi_byte_tx(67);
+	oi_byte_tx(16);
+	oi_byte_tx(65);
+	oi_byte_tx(16);
 }
 
